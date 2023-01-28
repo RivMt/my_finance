439d95f52565d1ee7c902c2c5a1f454e5195d5a3
San Kang <san.kang@outlook.com>
2023-01-28 18:40:25 +0900

    Add: API initiation code at `HomePage`
    It is used only for temporarily

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_api/my_api.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,9 +10,29 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  static const String _tag = "HomePage";
+
+  final client = FinanceClient();
+
+  void openPage(Widget page) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => page));
+  }
+
   /// Triggers on menu button pressed
   void onMenuPressed() {
+    init();
+  }
 
+  void init() async {
+    try {
+      final client = ApiClient();
+      await client.init(
+        onLoginRequired: () => openPage(const LoginPage()),
+      );
+    } on Exception catch(e) {
+      Log.e(_tag, "Error: $e");
+      return;
+    }
   }
 
   @override
