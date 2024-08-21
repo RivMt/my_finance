45e480c6264a0fe63ee789422046487b7b3f6056
San Kang <devrivmt@gmail.com>
2024-08-21 20:47:26 +0900

    Fix: `base-href` related bugs

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:my_finance/my_app.dart';
 
 void main() async {
-  // Lazy
+  // Init
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Localizations
   await EasyLocalization.ensureInitialized();
+  usePathUrlStrategy();
 
   // Run
   runApp(ProviderScope(
